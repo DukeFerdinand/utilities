4e5312eab4c689355408ad80a226dcd0672f5116
Douglass Hooker <DouglassHooker@gmail.com>
2019-05-21 14:57:39 -0400

    Add removal flag and handler

--- python-utils/hosts/main.py
+++ python-utils/hosts/main.py
@@ -9,33 +9,46 @@ parser.add_argument('--ip', type=str, required=False,
                     default='127.0.0.1', help="IP to associate with hostname. Defaults to 127.0.0.1")
 parser.add_argument('--file', type=str, required=False,
                     default="/etc/hosts", help="Override file to use for comparision/writing. Default is /etc/hosts")
+parser.add_argument('-r', action="store_true", dest="do_removal",
+                    help="Remove entry from /etc/hosts")
 
 args = parser.parse_args()
 
 
-class NewHost:
-    def __init__(self, hostname, ip):
-        self.hostname = hostname
-        self.ip = ip
+# If we don't specify to remove, assume write
+if not args.do_removal:
+    class NewHost:
+        def __init__(self, hostname, ip):
+            self.hostname = hostname
+            self.ip = ip
 
-    def host_string(self):
-        # Tab character to respect /etc/hosts spacing
-        return f"{self.ip}\t{self.hostname}"
+        def host_string(self):
+            # Tab character to respect /etc/hosts spacing
+            return f"{self.ip}\t{self.hostname}"
 
+    new_host = NewHost(args.hostname, args.ip)
 
-new_host = NewHost(args.hostname, args.ip)
-
-with open(args.file, 'r+') as host_file:
-    line = host_file.readline()
-    count = 1
-    while line:
-        line = line.strip()
-        if new_host.hostname in line:
-            raise Exception(
-                f"Hostname '{new_host.hostname}' already in use, please select a different name or modify the entry on line {count}")
-
+    with open(args.file, 'r+') as host_file:
         line = host_file.readline()
-        count += 1
-    host_file.write(new_host.host_string())
-    host_file.close()
-print("Successfully updated /etc/hosts")
+        count = 1
+        while line:
+            line = line.strip()
+            if new_host.hostname in line:
+                raise Exception(
+                    f"Hostname '{new_host.hostname}' already in use, please select a different name or modify the entry on line {count}")
+
+            line = host_file.readline()
+            count += 1
+        host_file.write(new_host.host_string())
+        host_file.close()
+    print("Successfully updated /etc/hosts")
+else:
+    print(f"Removing entry '{args.hostname}' from {args.file}")
+    with open(args.file, 'r+') as host_file:
+        lines = host_file.readlines()
+        host_file.seek(0)
+        for line in lines:
+            if args.hostname not in line:
+                host_file.write(line)
+        host_file.truncate()
+    print("Done!")
